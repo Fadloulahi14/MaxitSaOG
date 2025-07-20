896c200bf3744b0c8d0b38dc3225829886c56b2d
Fadloulahi14 <fallou.ndiaye22@isep-thies.edu.sn>
2025-07-20 14:33:19 +0000

    verion13

--- src/repository/AcceuilRepository.php
+++ src/repository/AcceuilRepository.php
@@ -2,14 +2,27 @@
 namespace App\Repository;
 use App\core\Database;
 use PDO;
-use App\core\App;
 
 class AcceuilRepository{
     private PDO $pdo;
     public function __construct(){
-        $this ->pdo = App::getDependency('database');
+        $this ->pdo = Database::getInstance();
 
     }
 
+   public function findAll(): array {
+        $sql = "SELECT * FROM commandes";
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+// public function findByClientId(int $clientId): array {
+    
+//     $sql = "SELECT * FROM commandes WHERE client_id = :client_id";
+//     $stmt = $this->pdo->prepare($sql);
+//     $stmt->bindParam(':client_id', $clientId, PDO::PARAM_INT);
+//     $stmt->execute();
+    
+//      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+// }
 
 }
