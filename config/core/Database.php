59c8bfb3b3f98f68e6f694f56cd0eee23c696a19
Fadloulahi14 <fallou.ndiaye22@isep-thies.edu.sn>
2025-07-26 16:10:13 +0000

    version27

--- config/core/Database.php
+++ config/core/Database.php
@@ -11,8 +11,6 @@ $dotenv->load();
 use PDO;
 use PDOException;
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../../'); // ou adapte le chemin selon ta structure
-$dotenv->load();
 
 class Database
 {
@@ -27,12 +25,29 @@ class Database
 // DB_NAME=GesAushan
 // DB_USER=gesaushan_user
 // DB_PASSWORD=gesaushan_password
-        $driver=$_ENV['DB_DRIVER'];
-        $host = $_ENV['DB_HOST'];
-        $port = $_ENV['DB_PORT'];
-        $dbname = $_ENV['DB_NAME'];
-        $user = $_ENV['DB_USER'];
-        $password = $_ENV['DB_PASSWORD'];
+
+// DB_DRIVER=pgsql
+// DB_HOST=trolley.proxy.rlwy.net
+// DB_PORT=55277
+// DB_NAME=railway
+// DB_USER=postgres
+// DB_PASSWORD=heocJkwrYBqAXgJOiBirQAFgJEuvMadZ
+// ROUTE_WEB=http://localhost:8000/
+
+
+        // $driver=$_ENV['DB_DRIVER'];
+        // $host = $_ENV['DB_HOST'];
+        // $port = $_ENV['DB_PORT'];
+        // $dbname = $_ENV['DB_NAME'];
+        // $user = $_ENV['DB_USER'];
+        // $password = $_ENV['DB_PASSWORD'];
+
+         $driver='pgsql';
+        $host ='trolley.proxy.rlwy.net';
+        $port ='55277';
+        $dbname ='railway';
+        $user ='postgres';
+        $password ='heocJkwrYBqAXgJOiBirQAFgJEuvMadZ';
 
         $dns = "$driver:host=$host; dbname=$dbname;port=$port";
 
